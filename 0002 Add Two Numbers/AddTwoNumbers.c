3a14f99f82da5fd17f4e2f3b1634505157789388
BusHeroVER <henry78925@gmail.com>
2022-04-14 23:16:03 +0800

    Add Two Numbers in C/C#

---
+++ 0002 Add Two Numbers/AddTwoNumbers.c
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2){
+    if (!l1)
+        return l2 ? NULL : l2;
+
+    struct ListNode *ret = l1;
+    int sum = l1->val + l2->val;
+    l1->val = sum % 10;
+    int tens = sum / 10;
+
+    while (l1->next && l2->next) {
+        l1 = l1->next;
+        l2 = l2->next;
+
+        sum = l1->val + l2->val;
+        sum += tens;
+        l1->val = sum % 10;
+        tens = sum / 10;
+    }
+
+    if (!l1->next) {
+        if (!l2->next) {
+            if (tens != 0) {
+                l1->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+                l1->next->val = tens;
+                l1->next->next = NULL;
+                return ret;
+            }
+            return ret;
+        }
+        l1->next = l2->next;
+    }
+
+    do {
+        l1 = l1->next;
+
+        sum = l1->val + tens;
+        l1->val = sum % 10;
+        tens = sum / 10;
+
+    } while (l1->next);
+    
+    if (tens != 0) {
+        l1->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+        l1->next->val = tens;
+        l1->next->next = NULL;
+    }
+    return ret;
+}
---
+++ 0002 Add Two Numbers/AddTwoNumbers.cs
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode AddTwoNumbers(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2 == null ? null : l2;
+        
+        ListNode ret = l1;
+        int sum = l1.val + l2.val;
+        l1.val = sum % 10;
+        int tens = sum / 10;
+
+        while (l1.next != null && l2.next != null) {
+            l1 = l1.next;
+            l2 = l2.next;
+
+            sum = l1.val + l2.val;
+            l1.val = sum % 10 + tens;
+            tens = sum / 10;
+        }
+
+        if (l1.next == null) {
+            if (l2.next == null) {
+                if (tens != 0)
+                    l1.next = new ListNode(tens);
+                return ret;
+            }
+            l1.next = l2.next;
+        }
+
+        do {
+            l1 = l1.next;
+
+            sum = l1.val + tens;
+            l1.val = sum % 10;
+            tens = sum / 10;
+
+        } while (l1.next != null);
+        
+        if (tens != 0)
+            l1.next = new ListNode(tens);
+        return ret;
+    }
+}
