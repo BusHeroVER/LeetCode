6570407d0d2726a21796c6bbdd90df374ae02fe1
BusHeroVER <henry78925@gmail.com>
2022-04-17 22:37:21 +0800

    Update in C

--- 0003 Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.c
+++ 0003 Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters.c
@@ -1,20 +1,21 @@
-#define MAX(a,b) (a>b)?a:b
+#define MAX(a, b) (a > b) ? a : b
 int lengthOfLongestSubstring(char * s){
     int s_len = strlen(s);
     if (s_len <= 1)
         return s_len;
 
-    int head = 0, ret = 1;
+    int head = 0, index = 0, ret = 0;
     int map[256] = {0};
-    map[s[0]] = 1;
     
-    for (int i = 1; i < s_len; i++) {
-        char c = s[i];
-        if (map[c] != 0) {
-            ret = MAX(i - head, ret);
-            head = MAX(head, map[c]);;
+    while (*s) {
+        if (map[*s] != 0) {
+            ret = MAX(index - head, ret);
+            head = MAX(head, map[*s]);
+            
+            if (s_len - head < ret)
+                return ret;
         }
-        map[c] = i + 1;
+        map[*s++] = ++index;
     }
     return MAX(s_len - head, ret);
 }
